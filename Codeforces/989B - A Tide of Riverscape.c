0759fe34f1478a1524df56df0a541699aceedc40
jade-yu <jade_yu@dlsu.edu.ph>
2018-11-21 00:46:09 +0800

    Codeforces 989B
    WA on test 9

---
+++ Codeforces/989B - A Tide of Riverscape.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, p;
+    scanf("%d %d", &n, &p);
+    
+    char N[n];
+    scanf("%s", N);
+    
+    int i = 0, flag = 0;
+	
+	while(flag == 0 && i + p < n) {
+		if(N[i] == '.') {
+			flag = 1;
+			if(N[i+p] == '.') {
+				N[i] = '1';
+			} else {
+					N[i] = '1';
+				else N[i] = '0';
+			}
+		} else {
+			if(N[i] == '.') {
+				flag = 1;
+				if(N[i] == '0')
+					N[i+p] = '1';
+				else N[i+p] = '0';
+			} else if(N[i] != N[i+p]) {
+				flag = 1;
+			}
+		}
+		
+		i++;
+	}
+	
+	if(flag == 1) {
+		i = 0;
+		
+		while(i < n) {
+			if(N[i] == '.') {
+				N[i] = '0';
+			}
+			
+			i++;
+		}
+		printf("%s", N);
+	} else printf("No");
+	
+    return 0;
+}
+
