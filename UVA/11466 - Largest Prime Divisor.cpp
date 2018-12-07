b453b2f18873666a136ea0fc2d03675a8f77f9a4
jade-yu <jade_yu@dlsu.edu.ph>
2018-12-07 19:18:01 +0800

    UVA 11466
    count up every time you get a prime factor, then get the latest number (given that you already have at least one existing prime factor)

---
+++ UVA/11466 - Largest Prime Divisor.cpp
@@ -0,0 +1,68 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef pair<int,ii> iii;
+
+#define MAX_N 40000000
+
+int main() {
+	bitset<MAX_N + 1> *sieve = new bitset<MAX_N + 1>();
+	vi primes;
+	for(ll i = 2; i <= MAX_N; i++) {
+		if(!sieve->test(i)) {
+			primes.push_back(i);
+			if(i <= (ll)sqrt(MAX_N)) {
+				for(ll j = i * i; j <= MAX_N; j += i) {
+					sieve->set(j);
+				}
+			}
+		}
+	}
+	
+	ll n = -1;
+	scanf("%lld",&n);
+	while(n != 0) {
+		vi pf;
+		if(n < 0) {
+			n *= -1;
+		}
+		
+		ll temp = n;
+		ll count = 0, max;
+		for(ll i = 0; i < primes.size() && primes[i] * primes[i] <= temp; i++) {
+			if(temp % primes[i] == 0) {
+				count++;
+				if(count > 0) {
+					max = primes[i];
+				}
+				while(temp % primes[i] == 0) {
+					temp /= primes[i];
+				}
+			}
+		}
+		if(temp != 1 && count > 0) {
+			max = temp;
+			count++;
+		}
+		
+		if(count > 1) {
+			printf("%lld\n",max);
+		} else {
+			printf("-1\n");
+		}
+		scanf("%lld",&n);
+	}
+}
